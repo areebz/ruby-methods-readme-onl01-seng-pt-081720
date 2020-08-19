9fe13ab2648c9e7d0c0b957f090d3c3bf442eea7
Areeba Zahir <areebazahir3@gmail.com>
2020-08-19 00:38:43 +0000

    Automatically backed up by Learn

--- lib/example.rb
+++ lib/example.rb
@@ -1,5 +1,4 @@
 def say_hello_world_ten_times
-
 phrase = "Hello World!"
 puts phrase
 puts phrase
@@ -11,3 +10,10 @@ puts phrase
 puts phrase
 puts phrase
 puts phrase
+end
+
+say_hello_world_ten_times
+
+# ...The rest of the program
+
+say_hello_world_ten_times
